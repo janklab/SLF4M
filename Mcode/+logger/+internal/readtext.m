fb5c3ed38a1b42082b4dd1e6b98b5c439b482e81
Andrew Janke <andrew@apjanke.net>
2021-01-16 03:39:50 -0500

    helptext

--- Mcode/+logger/+internal/readtext.m
+++ Mcode/+logger/+internal/readtext.m
@@ -1,4 +1,9 @@
 function out = readtext(file, encoding)
+% Read the contents of a text file as a string
+%
+% This is analagous to Matlab's readcsv and readtable, and exists because Matlab
+% doesn't provide a basic file-slurping mechanism.
+
 arguments
   file (1,1) string
   encoding (1,1) string = 'UTF-8' % TODO: auto-detect file encoding via sniffing
